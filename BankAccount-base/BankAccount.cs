7d7110b46ffd95d9a3ab44631b9653f44a607065
Hieke Keuning <12240799+hiekekeuning@users.noreply.github.com>
2025-10-08 21:49:31 +0200

    base files for testing

---
+++ BankAccount-base/BankAccount.cs
@@ -0,0 +1,50 @@
+﻿namespace BankAccount;
+
+public class BankAccount
+{
+    public string CustomerName { get; }
+    public decimal Balance { get; private set; }
+    public bool IsFrozen { get; private set; }
+
+    public BankAccount(string name, decimal balance)
+    {
+        this.CustomerName = name;
+        this.Balance = balance;
+    }
+
+    public void Debit(decimal amount)
+    {
+        if (IsFrozen)
+        {
+            throw new FrozenAccountException(); 
+        }
+        if (amount > Balance || amount < 0)
+        {
+            throw new ArgumentOutOfRangeException("amount"); // kan beter
+        }
+        Balance -= amount;
+    }
+    public void Credit(decimal amount)
+    {
+        if (IsFrozen)
+        {
+            throw new FrozenAccountException();
+        }
+        if (amount < 0)
+        {
+            throw new ArgumentOutOfRangeException("amount");
+        }
+        Balance += amount;
+    }
+
+    public void Freeze() => IsFrozen = true;
+  
+    public void Unfreeze() => IsFrozen = false;
+
+}
+
+public class FrozenAccountException : Exception
+{
+    
+}
+
---
+++ BankAccount-base/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿namespace BankAccountTests;
+using BankAccount;
+
+public class BankAccountTests
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+
+    [Fact]
+    public void DebitWithValidAmount()
+    {
+        // Arrange
+        BankAccount account = new("Hieke", 100);
+        decimal amount = 25;
+
+        // Act
+        account.Debit(amount);
+
+        // Assert
+        Assert.Equal(75, account.Balance);
+    }
+
+    [Fact]
+    public void DebitTriggerDebitException()
+    {
+        // Arrange
+        BankAccount account = new("Hieke", 100);
+        decimal amount = 125;
+
+        // Act
+        Action action = () => account.Debit(amount);
+
+        // Assert
+        Assert.Throws<ArgumentOutOfRangeException>(action);
+        
+
+    }
+}
+
+
